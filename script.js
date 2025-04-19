9447b34e9d9bc14f4f56221064e6a661aaa990e1
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-04-19 12:53:20 +0500

    learning about dom and dom manipulation

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 10;
+document.querySelector('.score').textContent = 21;
+document.querySelector('.highscore').textContent = 1;
+document.querySelector('.guess').value = 44;
+console.log(document.querySelector('.guess').value);
