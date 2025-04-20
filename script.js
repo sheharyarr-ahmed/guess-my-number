30cd465c3d560a8650af2643e74ac64ddd291486
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-04-20 05:51:57 +0500

    setting the functionality of the check button.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.number').textContent = 10;
-document.querySelector('.score').textContent = 21;
-document.querySelector('.highscore').textContent = 1;
-document.querySelector('.guess').value = 44;
-console.log(document.querySelector('.guess').value);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.number').textContent = 10;
+// document.querySelector('.score').textContent = 21;
+// document.querySelector('.highscore').textContent = 1;
+// document.querySelector('.guess').value = 44;
+// console.log(document.querySelector('.guess').value);
+document.querySelector('.check').addEventListener('click', function () {
+  //   console.log(document.querySelector('.guess').value);
+  //   document.querySelector('.message').textContent =
+  //     'going in the right direction';
+  const guess = document.querySelector('.guess').value;
+  console.log(guess);
+});
