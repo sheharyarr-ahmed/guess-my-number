505667abae327da26683d8acb9923cadd94ca67e
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-04-22 06:08:21 +0500

    successfully added too high, too low and correct number logic

--- script.js
+++ script.js
@@ -5,6 +5,9 @@
 // document.querySelector('.highscore').textContent = 1;
 // document.querySelector('.guess').value = 44;
 // console.log(document.querySelector('.guess').value);
+const secretNumber = Math.trunc(Math.random() * 20) + 1; //state variable also
+let score = 20; // this variable is called state variable as it holds the application state.
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   //   console.log(document.querySelector('.guess').value);
   //   document.querySelector('.message').textContent =
@@ -14,5 +17,26 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '❌ Not a number!'; //this is an scenario to tell how to react if someboady entered nothing and pressed on click.
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '✅ Correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '💩 You lost the game';
+      document.querySelector('.message').textContent = '📈 Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💩 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💩 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
