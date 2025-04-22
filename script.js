40bcb8b2bee6bd59b404f10ded2227c0450551ee
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-04-22 05:20:15 +0500

    adding game logics

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ document.querySelector('.check').addEventListener('click', function () {
   //   console.log(document.querySelector('.guess').value);
   //   document.querySelector('.message').textContent =
   //     'going in the right direction';
-  const guess = document.querySelector('.guess').value;
+  const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '❌ Not a number!'; //this is an scenario to tell how to react if someboady entered nothing and pressed on click.
+  }
 });
